d93e76ba7ed3f8eefe2b36acc9650998401ec890
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2025-03-22 21:39:56 -0400

    m5-w3-d2-exer

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Lists from "./Lists";
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        loading: false,
+        alldata: [],
+        singledata: {
+          title: "",
+          author: ""
+        }
+      };
+    }
+
+    getLists = () => {
+      fetch("http://localhost:5000/posts")
+      .then(res => res.json())
+      .then(result =>
+        this.setState({
+          loading: false,
+          alldata: result
+        })
+      )
+      .catch(console.log);
+    }
+
+    render() {
+      const listTable = this.state.loading ? (
+        <span>Loading Data.....Please be patient</span>
+      ) : (
+        <Lists alldata = {this.state.alldata}/>
+      );
+      return (
+        <div className="container">
+          <span className="title-bar">
+            <button
+            type="button"
+            className="btn btn-primary"
+            onClick={this.getLists}
+            >
+            
+              | Get List
+            </button>
+          </span>
+          {listTable}
+        </div>
+      )
+    }
+  }
+
+
+
+export default App;
